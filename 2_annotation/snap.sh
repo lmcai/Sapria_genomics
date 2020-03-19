b8958125d01be4512ab1e034b6652de8390954e3
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-03-19 17:27:53 -0400

    Update snap.sh

--- 2_annotation/snap.sh
+++ 2_annotation/snap.sh
@@ -3,7 +3,11 @@ maker2zff -x 0.5 -l 50 Sapria_longintron.rnd1.all.gff
 
 fathom genome.ann genome.dna -gene-stats > gene-stats.log 2>&1
 fathom genome.ann genome.dna -validate > validate.log 2>&1
+#filter genome.ann and genome.dna based on validate.log
+
+# collect the training sequences and annotations, plus 1000 surrounding bp for training
 fathom -categorize 1000 genome.ann genome.dna > categorize.log 2>&1
+#uni.* only contains regions pass validation
 fathom -export 1000 -plus uni.ann uni.dna 
 mkdir params
 cd params
