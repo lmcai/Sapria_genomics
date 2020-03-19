bb5895a5ef4e56fd15b8ffd6ecd7645711d7b5c4
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-03-19 13:49:33 -0400

    Update busco.sh

--- 1_validation_of_genome_assembly/busco/busco.sh
+++ 1_validation_of_genome_assembly/busco/busco.sh
@@ -1,6 +1,7 @@
 module load centos6
 module load BUSCO/3.0.2-fasrc01
 #compare sapria assembly to plant BUSCO
-python ~/programs/busco/scripts/run_BUSCO.py -i Sapria_V1.softmasked.fa -o  Sapria_V1 -l /n/scratchlfs/davis_lab/lmcai/embryophyta_odb9 -m genome -c 8
+python ~/programs/busco/scripts/run_BUSCO.py -i Sapria_V1.softmasked.fa -o sapria.plant -l /n/scratchlfs/davis_lab/lmcai/embryophyta_odb9 -m genome -c 8
 
 #compare sapria assembly to eukaryote BUSCO
+python ~/programs/busco/scripts/run_BUSCO.py -i Sapria_V1.softmasked.fa -o sapria.eukaryote -l /n/scratchlfs/davis_lab/lmcai/eukaryota_odb10 -m genome -c 8
