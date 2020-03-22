8e86dc556e019cac1c6b039ecc52363d9e1a3755
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-03-21 23:13:49 -0400

    Delete jellyfish.sh

--- 0_sapria_assembly/kmer/jellyfish.sh
+++
@@ -1,5 +0,0 @@
-#load module
-module load jellyfish/2.2.5-fasrc01
-#run jellyfish to count kmers, here kmer size of 27 is used
-jellyfish count -t 16 -C -m 27 -s 16G -g generators -G 9 -o 27mer_out --min-qual-char=?
-jellyfish histo -o 27mer_out.histo 27mer_out
