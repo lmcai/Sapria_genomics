6d3cd3b07a523ff2a403f4e254ad514b8042b6da
Liming Cai <daybreak.chua@gmail.com>
2020-03-29 15:38:27 -0400

    Update unmapped_reads_extraction_and_assembly.sh

--- 1_validation_of_genome_assembly/unmapped_reads/unmapped_reads_extraction_and_assembly.sh
+++ 1_validation_of_genome_assembly/unmapped_reads/unmapped_reads_extraction_and_assembly.sh
@@ -1,24 +1,24 @@
 ################################
 #mapping illumina reads to assembly
-bwa mem -t32 -p -C links16.scaffolds.fa ../2_longranger_basic_barcoded_fq/outs/barcoded.fastq.gz | samtools sort -@16 -t -o links16.reads.sort.bam
-
+bwa mem -t32 -p -C Sapria_V1.fa ../2_longranger_basic_barcoded_fq/outs/barcoded.fastq.gz | samtools sort -@16 -t -o Sapria_V1.reads.sort.bam
+		
 ################################
 #extract unmapped reads with samtools
 module load samtools/1.5-fasrc02
 #get unmapped reads
-samtools view -f 4 -b supernova.assemblyV0V1.reads.sortbx.bam | samtools bam2fq >supernova.assemblyV0V1.unmapped.fastq
+samtools view -f 4 -b Sapria_V1.reads.sort.bam | samtools bam2fq >Sapria_V1.unmapped.fastq	
 
 ################################
 #Reads error correction
 module load TrimGalore/0.5.0-fasrc01
 module load cutadapt/1.8.1-fasrc01
-trim_galore -q 5 --phred33 --length 36 --stringency 5 tetra.m500.unmapped.fastq
+trim_galore -q 5 --phred33 --length 36 --stringency 5 Sapria_V1.unmapped.fastq
 
 
 ################################
 #Assembly with Abyss
 module load gcc/4.9.3-fasrc01 abyss/1.5.2-fasrc01
-abyss-pe k=20 name=unaligned se=timglore/supernova.assemblyV0V1.unmapped_trimmed.fq
+abyss-pe k=20 name=unaligned se=Sapria_V1.unmapped_trimmed.fq
 
 #output summary
 n       n:500   n:N50   min     N80     N50     N20     E-size  max     sum     name
